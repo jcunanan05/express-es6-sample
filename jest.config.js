77c26b96194d9c8a28b7c10d09cca436c80a28b1
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-08 01:50:14 -0500

    Enable Route testing with async await
    Add regenerator runtime in jest config. see this [closed issue](https://github.com/facebook/jest/issues/5698)

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,3 @@
-module.exports = {};
+module.exports = {
+  setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime']
+};
--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,15 @@
+import request from 'supertest';
+import app from '../app';
+
 describe('sample test', () => {
   it('jest works', () => {
     expect(true).toBe(true);
+  });
+});
+
+describe('index route', () => {
+  it('responds 200', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
   });
 });
