3313831ca444de64a1df61d4f5bdd6cf5359ae03
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-07 22:12:53 -0500

    Convert cjs import to es6 imports

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+import express from 'express';
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
