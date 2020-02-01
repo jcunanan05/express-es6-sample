9eccb223afb27403f40b143c8411d249408be9da
jcunanan05 <cunanan.jonathan1995@gmail.com>
2020-02-01 17:37:23 -0500

    Move express generated files to server/

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
